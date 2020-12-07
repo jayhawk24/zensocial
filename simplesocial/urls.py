57cca1ba31e273666a27656ef02eb83884cfc00c
jayhawk24 <anubhavthrilling24@gmail.com>
2020-12-08 02:52:20 +0530

    index

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -20,11 +20,10 @@ from django.views.generic.base import RedirectView
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='groups'), name="home"),
+    path('', views.HomePage.as_view(), name="home"),
     path('accounts', include('accounts.urls', namespace='accounts')),
     path('accounts', include('django.contrib.auth.urls')),
     path("test", views.TestPage.as_view(),name="test"),
-    path("thanks",views.ThanksPage.as_view(),name="thanks"),
     path("thanks",views.ThanksPage.as_view(),name="thanks"),
     path('posts', include(('posts.urls'), namespace='posts')),
     path('groups', include('groups.urls', namespace='groups')),
